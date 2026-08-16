99f9da2bae041cd76912bd36ddc6bb0d307495c1
bayadrien <bayadrien@hotmail.fr>
2026-08-16 17:03:17 +0200

    Modernise l atelier de deck

--- app/api/deck/[id]/route.ts
+++ app/api/deck/[id]/route.ts
@@ -27,7 +27,6 @@ export async function GET(
               collections: {
                 where: {
                   userId,
-                  variant: "normal",
                 },
               },
             },
